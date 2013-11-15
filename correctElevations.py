cd748a7ab846736a5e036186e9011ea822f7ec85
Wyatt Pearsall <wyatt.pearsall@water.ca.gov>
2013-11-15 09:59:11 -0800

    single -> featureclass and elevation filtering finished

---
+++ correctElevations.py
@@ -0,0 +1,109 @@
+import arcpy, sys
+
+def getModeAvg (arr):
+  counts = {}
+  first = 0,0
+  second = 0,0
+  third = 0,0
+  for line in arr:
+    num = getElev(line)
+    roundNum = int(round(num*100))
+    if counts.get(roundNum):
+      counts[roundNum]+=1
+    else:
+      counts[roundNum]=1
+  for key, val in counts.iteritems():
+    if val > first[1]:
+      third = second
+      second = first
+      first = key,val
+    elif val > second:
+      third = second
+      second = key,val
+    elif val > third:
+      third = key,val
+  #print first,second,third
+  #print abs(first[0] - second[0]) 
+  #print abs(third[0] - second[0])
+  #print abs(first[0] - third[0])
+
+  if not second[0]:
+    second = first
+  if not third[0]:
+    third = (first[0]*first[1]+second[0]*second[1])/(first[1]+second[1]), (first[1]+second[1])/2.
+  if abs(first[0] - second[0]) > 15 or abs(third[0] - second[0]) > 15 or abs(first[0] - third[0]) > 15:
+    return False
+  return (first[0]*first[1]+second[0]*second[1]+third[0]*third[1])/(first[1]+second[1]+third[1])/100.
+
+def filterOutliers (avg,lines,slope,spread):
+  cleaned = []
+  mid = len(lines)/2
+  for i,line in enumerate(lines):
+    elev = getElev(line)
+    corr = slope*(mid-i)
+    if abs(elev+corr-avg) <= spread:
+      cleaned.append(line)
+  return cleaned
+
+def getElev(line):
+  return float(line.split(',')[2])
+
+def run(infile, outfile, spread=0.1):
+  inp = open(infile)
+  out = open(outfile, 'w')
+  headers = inp.readline()
+  out.write(headers)
+  lines = [line for line in inp]
+  count = 0
+  chunks = []
+  start = 0
+  end = 100
+  diff = 100
+
+  while start < len(lines):
+#    print start,end
+    arr = lines[start:end]
+    avg = getModeAvg(arr)
+  #  print avg
+    if avg:
+      chunks.append((avg,(start+end)/2,arr))
+  #    print "advance", start,end,diff  
+      start, end = end, end+diff
+      if diff < 60:
+        diff*=2
+    #  print "advanced to", start,end,diff 
+    else:
+    #  print "failed", start,end,diff
+      diff = int((end-start)/2)
+      end = end-diff
+   #   print "failed to", start,end,diff
+
+  for i,chunk in enumerate(chunks):
+    if i == 0:
+      slope = (chunk[0]-chunks[i+1][0])/(chunk[1]-chunks[i+1][1])
+    elif i == len(chunks) -1:
+      slope = (chunks[i-1][0]-chunk[0])/(chunks[i-1][1]-chunk[1])
+    else:
+      #print chunks[i-1][0],chunks[i-1][1],chunk[0],chunk[1],chunks[i+1][0],chunks[i+1][1]
+      slope = ((chunks[i-1][0]-chunk[0])/(chunks[i-1][1]-chunk[1])+
+              (chunk[0]-chunks[i+1][0])/(chunk[1]-chunks[i+1][1]))/2
+    #print slope
+    cleaned = filterOutliers(chunk[0],chunk[2],slope,spread)
+    for line in cleaned:
+      out.write(line)
+      count+=1
+  inp.close()    
+  out.close()
+  arcpy.AddMessage("\n\nStripped "+str(len(lines)-count)+" rows from "+str(len(lines))+" collected. This is "+str((len(lines)-count*1.0)/len(lines)*100.)+"%.\n\n")
+
+if __name__ == "__main__":
+  if len(sys.argv) > 2:
+    run(sys.argv[1],sys.argv[2])
+  else:
+    input = raw_input("Input file: ")
+    output = raw_input("Output file: ")
+    run(input,output)
+
+#import random
+#(random.random()/25+1)*4 +num/4000.0 for num in range(1000)
+# for first iteration random files.. Now operates on csv's.. Need to csv these elevs if needed again
--- rawSingleBeamToFeatureClass.py
+++ rawSingleBeamToFeatureClass.py
@@ -1,35 +1,70 @@
 #! c:python26/ArcGIS10.0/python.exe
-import arcpy, os, sys
+import arcpy, os, sys, correctElevations
 arcpy.CheckOutExtension('3D')
 
+overwrite = "N"
+filterElevs = "Y"
+spread = 0.1
 
-if len(sys.argv) == 4:
-  input = sys.argv[1]
-  feat = sys.argv[2]
-  zone = sys.argv[3]
-else:
+if len(sys.argv) == 1:
   input = raw_input("Input file: ")
   feat = raw_input("Output feature class name: ")
   zone = raw_input("State Plane Zone: ")
+  overwrite = raw_input("Overwrite output? y/n: ")
+  filterElevs = raw_input("Filter elevations? y/n: ")
+  if filterElevs[:1].upper() =="Y":
+    spread = float(raw_input("Elevation spread: "))
+else:
+  args = len(sys.argv)
+  if args < 4:
+    arcpy.AddMessage("You must enter a valid input file, feature class name, and State Plane Zone.")
+    exit(1)
+  input = sys.argv[1]
+  feat = sys.argv[2]
+  zone = sys.argv[3]
+  if args > 4:
+    overwrite = sys.argv[4]
+  if args > 5:  
+    filterElevs = sys.argv[5]
+  if args > 6:
+    spread = float(sys.argv[6])
+
+if not input or not feat or not zone:
+  arcpy.AddMessage("You must enter a valid input file, feature class name, and State Plane Zone.")
+  exit(1)
 
 inpPath = os.path.realpath(input)
 pathArr = inpPath.split('\\')
 gdbPath = ''
+if overwrite[:1].upper() =="Y" or overwrite == True:
+  arcpy.env.overwriteOutput = True
+  arcpy.AddMessage("Overwriting output: "+str(arcpy.env.overwriteOutput))
+
 
 if pathArr[0].find(':'):
   del pathArr[0]
-  inpPath = '\\\\'+'\\'.join(pathArr)
+  inpPath = '\\'+'\\'.join(pathArr)
 
 for i, piece in enumerate(pathArr):
-  if piece == 'DATA_OTHER':
-    gdbPath = ('\\\\'+'\\'.join(pathArr[:i])+'\\DATA_GIS\\Data.gdb')
+  if piece.upper() == 'DATA_OTHER':
+    gdbPath = ('\\'+'\\'.join(pathArr[:i])+'\\DATA_GIS\\Data.gdb')
     break
+
+if inpPath[:4].upper() == '\PCD':
+  inpPath = '\\'+inpPath
+if gdbPath[:4].upper() == '\PCD':
+  gdbPath = '\\'+gdbPath
 
 if gdbPath == '':
   raise IOError("This script can only operate on files in DATA_OTHER folders. Follow the folder structure.")
 inp = open(inpPath)
 out = open('TEMPFILE.csv','w');
-sr = r'C:\Program Files (x86)\ArcGIS\Desktop10.0\Coordinate Systems\Projected Coordinate Systems\State Plane\NAD 1983 (US Feet)\NAD 1983 StatePlane California III FIPS 0403 (US Feet).prj'
+
+
+if len(zone)<4:
+  sr = r'C:\Program Files (x86)\ArcGIS\Desktop10.0\Coordinate Systems\Projected Coordinate Systems\State Plane\NAD 1983 (US Feet)\NAD 1983 StatePlane California III FIPS 0403 (US Feet).prj'
+else:
+  sr = zone
 if zone == '2' or zone == 'II':
   sr.replace('III FIPS 0403','II FIPS 0402')
 
@@ -45,8 +80,18 @@ for line in inp:
 inp.close()
 out.close()
 
-arcpy.MakeXYEventLayer_management('TEMPFILE.csv','x','y','temppoint',sr,'bottom_elevation')
+
+if filterElevs[:1].upper() =="Y" or filterElevs == True:
+  correctElevations.run('TEMPFILE.csv','TEMPFILE2.csv',spread)
+  arcpy.MakeXYEventLayer_management('TEMPFILE2.csv','x','y','temppoint',sr,'bottom_elevation')
+else:
+  arcpy.MakeXYEventLayer_management('TEMPFILE.csv','x','y','temppoint',sr,'bottom_elevation')
+
 
 arcpy.FeatureClassToFeatureClass_conversion("temppoint",gdbPath,feat)
 
 arcpy.Delete_management('temppoint')
+try:
+  os.unlink("TEMPFILE.csv")
+  os.unlink("TEMPFILE2.csv")
+except OSError: pass 

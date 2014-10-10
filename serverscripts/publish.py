d7f84acfb1e13e0a476fb5f5c46dee5a0d78ec2c
Wyatt Pearsall <wyatt.pearsall@water.ca.gov>
2014-10-10 12:19:36 -0700

    Updated to allow first time publishing

--- serverscripts/publish.py
+++ serverscripts/publish.py
@@ -12,14 +12,15 @@ import getpass
 
 def main(argv=None):
 
-  def doRequest(url,params):
-    httpConn = httplib.HTTPConnection(serverName,serverPort)
+
+  def doRequest(name, url, params):
+    httpConn = httplib.HTTPConnection(name,serverPort)
     httpConn.request("POST", url, params, headers)
 
     response = httpConn.getresponse()
     if (response.status != 200):
       httpConn.close()
-      print("Couldn't access the service {} in {} on {}. Check if it exists".format(name,serverFolder,serverName))
+      print("Couldn't access the service {} on {}. Check if it exists".format(url,name))
       return None
     data = response.read()
     httpConn.close()
@@ -29,26 +30,36 @@ def main(argv=None):
     return data
 
 
+
   username = raw_input("Enter user name: ") or 'wpearsal'
   password = getpass.getpass("Enter password: ")
+
   stagingName = raw_input("Enter Staging Server name (mrsbmapp...): ") or 'mrsbmapp21158'
-  prodName = raw_input("Enter Production Server name (mrsbmapp..._: ") or 'mrsbmapp21188'
+  stagingFolder = raw_input("Enter Staging Folder: ") or "cadre"
+
+  prodName = raw_input("Enter Production Server name (mrsbmapp...: ") or 'mrsbmapp21188'
+  prodFolder = raw_input("Enter Production Folder: ") or "Public"
   serverPort = 6080
+
+
 
   print("\nGetting token...\n")
 
-  stagingToken = getToken(username,password,stagingName)
-  prodToken = getToken(username,password,prodName,serverPort)
-  if token == None:
-    print("Could not generate a token with the username and password provided.")
+  stagingToken = getToken(username, password, stagingName, serverPort)
+  prodToken = getToken(username, password, prodName, serverPort)
+  if stagingToken == None or prodToken == None:
+    print("Could not generate valid tokens with the username and password provided.")
     return
 
-  params = urllib.urlencode({'token': token, 'f': 'json'})
+  stagingParams = urllib.urlencode({'token': stagingToken, 'f': 'json'})
+  prodParams = urllib.urlencode({'token': prodToken, 'f': 'json'})
   headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+
+
 
   folderPath = path.abspath(raw_input("Enter the path to the folder of MXDs: ") or '.')
   matchParam = raw_input("Enter filter for MXD names (or nothing for all MXDs): ")
-  serverFolder = raw_input("Enter service folder: ") or "Public"
+
   gisConnection = raw_input("Enter name of GIS Server connection: ") or "prod"
   overrideProps = raw_input("Enter JSON updates to service properties: ")
 
@@ -73,7 +84,7 @@ def main(argv=None):
                                              "ARCGIS_SERVER",
                                              "",
                                              False,
-                                             serverFolder
+                                             prodFolder
                                               )
     if analysis['errors'] == {}:
       print("Making Service Definition: {}.\n".format(sd))
@@ -83,13 +94,19 @@ def main(argv=None):
       print("Errors creating service definition",analysis['errors'])
       continue
 
-    serviceURL = "/arcgis/admin/services/" + serverFolder + "/" + name + ".MapServer"
-    
+
+
+    stagingURL = "/arcgis/admin/services/" + stagingFolder + "/" + name + ".MapServer"
+    prodURL = "/arcgis/admin/services/" + prodFolder + "/" + name + ".MapServer"
+
+
 
     print("Getting service parameters.\n")             
-    data = doRequest(serviceURL,params)
+    data = doRequest(stagingName, stagingURL, stagingParams)
     if not data:
       continue
+
+
 
     if overrideProps != "":
       overrideJSON = json.loads(overrideProps)
@@ -100,21 +117,27 @@ def main(argv=None):
     print("Service Parameters saved.\n")
 
 
+
     print("Removing old service.\n")
-    if not doRequest(serviceURL + "/delete",params):
-      continue
-    print("Service Removed.\n")
+    try:
+      doRequest(prodName, prodURL + "/delete", prodParams)
+      print("Service Removed.\n")
+    except:
+      print("No service at {}. This must be the first time it's been published. Make sure you've analyzed it in ArcMap.\n".format(prodURL))
+
 
 
     print("Publishing new service.\n")
     arcpy.UploadServiceDefinition_server(sd,"GIS Servers/"+gisConnection)
-    print("{} published to {} folder on {}\n".format(name,serverFolder,serverName))
+    print("{} published to {} folder on {}\n".format(name,prodFolder,prodName))
+
 
 
     print("Adding saved properties.\n")
-    if not doRequest(serviceURL+"/edit",urllib.urlencode({'token': token, 'service':data, 'f': 'json'})):
+    if not doRequest(prodName, prodURL+"/edit", urllib.urlencode({'token': prodToken, 'service':data, 'f': 'json'})):
       continue
     print("Properties added.\n")
+
 
 
     print("{} successfully updated.\n".format(name))
